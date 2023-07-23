60ef90cceca943261d9df97dd5d49c1ba56aaaff
ILXYZ <ikigailabs@proton.me>
2023-07-24 00:49:11 +0200

    update

--- components/ETHereum.tsx
+++ components/ETHereum.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css';
 
 const EthereumComponent: React.FC = () => {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/ethereum')
@@ -12,8 +13,13 @@ const EthereumComponent: React.FC = () => {
       })
       .catch(error => {
         console.error(error);
+        setError("Failed to fetch data");
       });
   }, []);
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
 
   if (!data) {
     return <p>Loading...</p>;
