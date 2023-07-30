a97804dc8a9854c0414a7711737838d3df7f521e
ILXYZ <ikigailabs@proton.me>
2023-07-30 12:55:07 +0200

    update

--- pages/lidopage.tsx
+++ pages/lidopage.tsx
@@ -1,17 +1,13 @@
 // pages/LidoPage.tsx
 
 import React from 'react';
-import Head from 'next/head';
 import LidoInfo from '../components/LidoInfo';
 import styles from '../styles/Home.module.css';
 
 const LidoPage: React.FC = () => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Lido (LDO) Information</title>
-        <meta name="description" content="Lido (LDO)" />
-      </Head>
+
 
       <main className={styles.main}>
         <LidoInfo />
